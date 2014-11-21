5c874c0c9ad0bc848664e2abf63fecf277c91ca5
Vasya Pypkin <vasy@ukr.net>
2014-11-21 11:16:33 +0200

    Initial Commit

---
+++ Cinema2/Chair.cs
@@ -0,0 +1,15 @@
+namespace CinemaLib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    /// <summary>Коллекция кресел в зале</summary>
+    public class Chair
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+}
